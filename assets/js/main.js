a738f1ce00b14669f3af26093adfdd33eca21c8c
Jasper <jasperstolwijk@icloud.com>
2019-01-19 15:55:48 +0100

    Finished website
    The finished website from a private reposity into a public repository.

---
+++ assets/js/main.js
@@ -0,0 +1,39 @@
+// Hamburger responsive menu
+function hamburgerMenu() {
+  var x = document.getElementById("nav");
+  if (x.className === "nav") {
+    x.className += " responsive";
+  } else {
+    x.className = "nav";
+  }
+}
+
+//Smooth scroll
+  $(document).on('click', 'a[href^="#"]', function(e) {
+    var id = $(this).attr('href');
+    var $id = $(id);
+    if ($id.length === 0) {
+        return;
+    }
+    // prevent standard hash navigation (avoid blinking in IE)
+    e.preventDefault();
+    var pos = $id.offset().top;
+    // animated top scrolling
+    $('body, html').animate({scrollTop: pos});
+  });
+
+
+  //Textarea auto-expand
+  $(document)
+    .one('focus.autoExpand', 'textarea.autoExpand', function(){
+        var savedValue = this.value;
+        this.value = '';
+        this.baseScrollHeight = this.scrollHeight;
+        this.value = savedValue;
+    })
+    .on('input.autoExpand', 'textarea.autoExpand', function(){
+        var minRows = this.getAttribute('data-min-rows')|0, rows;
+        this.rows = minRows;
+        rows = Math.ceil((this.scrollHeight - this.baseScrollHeight) / 16);
+        this.rows = minRows + rows;
+    });
